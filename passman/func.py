f5b49001fedf2b521f905c3ee8f1aee59ea18d8f
AaravHattangadi <69454819+AaravHattangadi@users.noreply.github.com>
2021-09-25 20:33:30 +0530

    Further development on func.py

--- passman/func.py
+++ passman/func.py
@@ -3,6 +3,7 @@ import os
 from cryptography.fernet import Fernet
 
 # functions
+    # keys
 def writeKey():
     """
     Generates a key and save it into a file
@@ -10,6 +11,12 @@ def writeKey():
     key = Fernet.generate_key()
     with open("db/key.key", "wb") as key_file:
         key_file.write(key)
+
+def loadKey():
+    """
+    Loads the key from the current directory named `key.key`
+    """
+    return open("db/key.key", "rb").read()
 
 
 def setMasPass():
@@ -33,6 +40,25 @@ def setMasPass():
     print("A unique key has been generated in the 'db' folder. Use it to decrypt your passwords if you ever forget your master password.")
 
 def savePass():
+    key = loadKey()
+    fernet = Fernet(key)
+
     print('Enter Name of Password:')
     name = input()
+    print("Enter password")
+    Pass = input()
+    passEncoded = Pass.encode()
+    encrypted = fernet.encrypt(passEncoded)
+    cmd_command = "cd db/ && echo {} > {}.mpf".format(encrypted, name)
+    os.system(cmd_command)
+
+def readPass():
+    key = loadKey()
+    fernet = Fernet(key)
+    
+    print("Enter name of password to be read")
+    rName = "db/" + input() + ".mpf"
+    file = open(rName, "r")
+    fileCon = file.read().encode()
+    decrypted = fernet.decrypt(fileCon)
     
