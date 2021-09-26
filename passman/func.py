0bf7ed3c4b8aec7690a45cb6833bc147df33356e
AaravHattangadi <69454819+AaravHattangadi@users.noreply.github.com>
2021-09-26 16:03:46 +0530

    Added functionality and fixed bugs

--- passman/func.py
+++ passman/func.py
@@ -2,6 +2,9 @@ import hashlib
 import os
 from cryptography import fernet
 from cryptography.fernet import Fernet
+import time
+import secrets
+import string
 
 # functions
     # keys
@@ -83,11 +86,8 @@ def readpassMAS():
     uimas = input()
     with open("db/masterpassword.mpf", "r") as file:
         mpk = file.readline()
-        uimasHashed = hashlib.sha256(uimas.encode("utf-8")).hexdigest()
-        print(mpk)
-        print(uimasHashed)
-
-    if(mpk == uimasHashed):
+        uimasHashed = str(hashlib.sha256(uimas.encode("utf-8")).hexdigest())
+    if(mpk.strip() == uimasHashed):
         print("Enter name of password to be read")
         rName = "db/" + input() + ".mpf"
         file = open(rName, "r")
@@ -96,8 +96,95 @@ def readpassMAS():
         print(decrypted.decode("utf-8"))
 
     else:
-        print("<!ERR!> **invalid master password**")
+        #print("<!ERR!> **invalid master password**")
+        raise ValueError("Invalid master password")
+
+def savepassMAS():
+    key = loadKey()
+    fernet = Fernet(key)
+
+    print("Enter master password")
+    uimas = input()
+    with open("db/masterpassword.mpf", "r") as file:
+        mpk = file.readline()
+        uimasHashed = str(hashlib.sha256(uimas.encode("utf-8")).hexdigest())
+
+    if(mpk.strip() == uimasHashed):
+        print("Enter name of password")
+        name = input()
+        print("Enter password")
+        Pass = input()
+        passEncoded = Pass.encode()
+        encrypted = fernet.encrypt(passEncoded)
+        with open("db/" + name + ".mpf", "wb") as f:
+            f.write(encrypted)
+
+    else:
+         raise ValueError("Invalid master password")
 
 
-readpassMAS()
-    
+def deletepassMAS():
+    key = loadKey()
+    fernet = Fernet(key)
+
+    print("Enter master password")
+    uimas = input()
+    with open("db/masterpassword.mpf", "r") as file:
+        mpk = file.readline()
+        uimasHashed = str(hashlib.sha256(uimas.encode("utf-8")).hexdigest())
+
+    if(mpk.strip() == uimasHashed):
+        print("Enter name of password to delete")
+        pasName = input()
+        if input("Are you sure you want to delete the password of {} (y/n)".format(pasName)) != "y":
+            raise ValueError("Operation cancelled by user")
+            exit()
+        else:
+            if(os.path.exists("db/" + pasName + ".mpf")):
+                os.remove("db/" + pasName + ".mpf")
+                time.sleep(3)
+                if(os.path.exists("db" + pasName + ".mpf")):
+                    print("Password has been deleted")
+                else:
+                    raise ValueError("Unknown Error: Password Cannot be Deleted.")
+
+            else:
+                raise ValueError("Path does not exist")
+    else:
+        raise ValueError("Invalid Master Password")
+
+def generatePassword():
+    print("Input length of password")
+    length = int(input())
+    if length <8:
+        raise ValueError("Length of password cannot be shorter than 8 characters")
+    elif length >=8:
+        alphabet = string.ascii_letters + string.digits
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        print(password)
+        time.sleep(1)
+        if input("Do you want to save this password? (y/n)") != "y":
+            exit()
+        else:
+            key = loadKey()
+            fernet = Fernet(key)
+
+            print("Enter master password")
+            uimas = input()
+            with open("db/masterpassword.mpf", "r") as file:
+                mpk = file.readline()
+                uimasHashed = str(hashlib.sha256(uimas.encode("utf-8")).hexdigest())
+
+            if(mpk.strip() == uimasHashed):
+                print("Enter name of password")
+                name = input()
+                Pass = password
+                passEncoded = Pass.encode()
+                encrypted = fernet.encrypt(passEncoded)
+                with open("db/" + name + ".mpf", "wb") as f:
+                    f.write(encrypted)
+
+            else:
+                 raise ValueError("Invalid master password")
+    else:
+        raise ValueError("Unhandled Exception Error")
