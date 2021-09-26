3336df88a878cbee308cf6e1a7e39735c769feb8
AaravHattangadi <69454819+AaravHattangadi@users.noreply.github.com>
2021-09-26 13:31:05 +0530

    Improved functions

--- passman/func.py
+++ passman/func.py
@@ -1,23 +1,17 @@
 import hashlib
 import os
+from cryptography import fernet
 from cryptography.fernet import Fernet
 
 # functions
     # keys
 def writeKey():
-    """
-    Generates a key and save it into a file
-    """
     key = Fernet.generate_key()
     with open("db/key.key", "wb") as key_file:
         key_file.write(key)
 
 def loadKey():
-    """
-    Loads the key from the current directory named `key.key`
-    """
     return open("db/key.key", "rb").read()
-
 
 def setMasPass():
     print('''
@@ -37,20 +31,37 @@ def setMasPass():
     os.system(cmd_command)
     writeKey()
     print("Master Password has Been Set")
-    print("A unique key has been generated in the 'db' folder. Use it to decrypt your passwords if you ever forget your master password.")
+    print("A unique key has been generated in the 'db' folder. Use it to **manually** decrypt your passwords if you ever forget your master password.")
+
+# def savePass():
+#     file = open("db/masterpassword.mpf", "r")
+#     hash = file.read
+#     print("Enter master password")
+#     uimas = input()
+#     key = loadKey()
+#     fernet = Fernet(key)
+
+#     print('Enter Name of Password:')
+#     name = input()
+#     print("Enter password")
+#     Pass = input()
+#     passEncoded = Pass.encode()
+#     encrypted = fernet.encrypt(passEncoded)
+#     cmd_command = "cd db/ && echo {} > {}.mpf".format(encrypted, name)
+#     os.system(cmd_command)
 
 def savePass():
     key = loadKey()
     fernet = Fernet(key)
 
-    print('Enter Name of Password:')
+    print("Enter name of password")
     name = input()
     print("Enter password")
     Pass = input()
     passEncoded = Pass.encode()
     encrypted = fernet.encrypt(passEncoded)
-    cmd_command = "cd db/ && echo {} > {}.mpf".format(encrypted, name)
-    os.system(cmd_command)
+    with open("db/" + name + ".mpf", "wb") as f:
+        f.write(encrypted)
 
 def readPass():
     key = loadKey()
@@ -59,6 +70,34 @@ def readPass():
     print("Enter name of password to be read")
     rName = "db/" + input() + ".mpf"
     file = open(rName, "r")
-    fileCon = file.read().encode()
+    fileCon = file.read().encode("utf-8")
     decrypted = fernet.decrypt(fileCon)
+    print(decrypted.decode("utf-8"))
+
+
+def readpassMAS():
+    key = loadKey()
+    fernet = Fernet(key)
+
+    print("Enter master password")
+    uimas = input()
+    with open("db/masterpassword.mpf", "r") as file:
+        mpk = file.readline()
+        uimasHashed = hashlib.sha256(uimas.encode("utf-8")).hexdigest()
+        print(mpk)
+        print(uimasHashed)
+
+    if(mpk == uimasHashed):
+        print("Enter name of password to be read")
+        rName = "db/" + input() + ".mpf"
+        file = open(rName, "r")
+        fileCon = file.read().encode("utf-8")
+        decrypted = fernet.decrypt(fileCon)
+        print(decrypted.decode("utf-8"))
+
+    else:
+        print("<!ERR!> **invalid master password**")
+
+
+readpassMAS()
     
